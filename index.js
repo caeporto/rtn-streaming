5004278a5cb1888aa3b05170deba93d847a24ab6
Caetano Porto da Silva <caetanop@dbserver.com.br>
2021-09-26 23:49:50 -0300

    Fixing indexjs entry file

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-module.exports = function () {
-    let rtsp_server = require('./rtsp/rtsp_server');
-    rtsp_server = new rtsp_server();
-    return rtsp_server;
-}
+module.exports = new (require('./rtsp/rtsp_server'))();
